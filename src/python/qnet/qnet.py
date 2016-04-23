8a7e815264c42d0f87892261bfd2ca1ad015bae2
cpatdowling <cpatdowling@gmail.com>
2016-04-22 22:39:28 -0700

    updating src qnet

--- src/python/qnet/qnet.py
+++ src/python/qnet/qnet.py
@@ -6,6 +6,9 @@ class parameters:
         self.SIMULATION_TIME = 1000
         self.TIME_RESOLUTION = 0.001
         self.DRIVE_TIME = 1.0
+        self.BLOCKS_BEFORE_QUIT = 0.0
+        self.GARAGE_PROB = 0.0
+        self.GARAGE_NEIGHBOR_EFFECT = False
         
         #Network parameters
         self.EXOGENOUS_RATE = 1.5 
@@ -14,10 +17,11 @@ class parameters:
         self.NUM_SPOTS = 5
         #If param file passes single float, float is diagonalized across a network
         #matrix--not efficient use of memory but helps with later steady state computation
-        self.diagonalize = ["EXOGENOUS_RATE", "SERVICE_RATE", "RENEGE_TIME", "NUM_SPOTS"]
+        self.diagonalize = ["EXOGENOUS_RATE", "SERVICE_RATE", "RENEGE_TIME", "NUM_SPOTS", "GARAGE_NETWORK"]
         
         self.ROAD_NETWORK = ""
-        
+        self.GARAGE_NETWORK = ""
+
         #read parameter file
         try:
             inFile = open(inFilePath, 'r')
@@ -39,7 +43,7 @@ class parameters:
             print(err)
        
 class blockface:
-    def __init__(self, index=False, lambd=1.0, mu=2.0, renege=0.0, num_spots=5):
+    def __init__(self, index=False, lambd=1.0, mu=2.0, renege=0.0, num_spots=5, garage = False):
         self.i = index
         self.arrival_rate = lambd
         self.service_rate = mu
@@ -48,6 +52,10 @@ class blockface:
         self.spots = np.array([0.0 for j in range(int(num_spots))])
         self.neighbors = []
         self.neighbor_streets = {}
+        if int(garage) == 1 or garage == True:
+            self.garage = True
+        else:
+            self.garage = False
         
         #for calculating block face stats
         self.total = 0
@@ -56,6 +64,8 @@ class blockface:
         self.avg_park_time = 0.0
         self.utilization = []
         self.queue_length = []
+        self.isolated_rejects = 0
+        self.parking_garage_rejects = 0
 
 class street:
     def __init__(self, index, min_travel=1.0, max_travel=100.0):
@@ -101,19 +111,20 @@ class blockfaceNet:
         self.carIndex = 0
         self.trakers = []
         
+        self.all_spots = []
         for ii in range(num_blocks):
             self.bface[ii] = blockface(index=ii, lambd=self.params.EXOGENOUS_RATE[ii,ii],
                                        mu=self.params.SERVICE_RATE[ii,ii], 
                                        renege=self.params.RENEGE_TIME[ii,ii], 
-                                       num_spots = self.params.NUM_SPOTS[ii,ii])
+                                       num_spots = self.params.NUM_SPOTS[ii,ii], garage = self.params.GARAGE_NETWORK[ii,ii])
             neighboring_blocks = [ j for j, x in enumerate(self.params.ROAD_NETWORK[ii]) if x == 1 ]
             block_ids = [ i for i in range(len(neighboring_blocks)) ]
             self.bface[ii].neighbors = zip(block_ids, neighboring_blocks)
             
             for k in range(len(self.bface[ii].neighbors)):
                 self.bface[ii].neighbor_streets[k] = street(index = (ii, self.bface[ii].neighbors[k][1]))
-                
-        self.all_spots = [ self.bface[ii].spots for i in range(num_blocks) ]
+
+            self.all_spots.append(self.bface[ii].spots)
         
         self.streets = {}
         for origin in range(num_blocks):
@@ -121,6 +132,7 @@ class blockfaceNet:
             for dest_n in range(len(self.bface[origin].neighbors)):
                 self.streets[origin].append(self.bface[origin].neighbor_streets[dest_n].traffic)
                 
+        self.injection_b = np.diag(self.params.EXOGENOUS_RATE)
         injection_blocks = np.diag(self.params.EXOGENOUS_RATE)
         injection_block_indicies = [ i for i, x in enumerate(injection_blocks) if float(x) != 0.0 ]
         self.injection_map = {}
@@ -130,6 +142,10 @@ class blockfaceNet:
         
         self.total_flow = np.zeros(self.params.ROAD_NETWORK.shape)
         
+        for ii in range(num_blocks):
+            for k in range(len(self.bface[ii].neighbors)):
+                if self.params.GARAGE_NEIGHBOR_EFFECT == 1.0:
+                    self.bface[self.bface[ii].neighbors[k][1]].garage = True
         
     def step_time(self, supress=False):
         self.timer = self.timer + self.params.TIME_RESOLUTION  
@@ -153,8 +169,18 @@ class blockfaceNet:
             if self.timer % (self.params.SIMULATION_TIME / 100) <= self.params.TIME_RESOLUTION:
                 for block in self.bface.keys():
                     spots = float(self.bface[block].num_parking_spots)
-                    parked = float(len([ i for i, x in enumerate(self.all_spots[block]) if x > -1 * self.params.TIME_RESOLUTION ]))
+                    parked = float(len([ i for i, x in enumerate(self.all_spots[block]) if x >  self.params.TIME_RESOLUTION ]))
                     self.bface[block].utilization.append(parked/spots)
+                    if parked/spots > 1.0:
+                        print("\n\n")
+                        print("bface[block].spots")
+                        print(self.bface[block].spots)
+                        print("all_spots[block]")
+                        print(self.all_spots[block])
+                        print("num parked")
+                        print(parked)
+                        print("bface[block].num_parking_spots")
+                        print(self.bface[block].num_parking_spots)
                     
         if "queue-length" in self.stats:
             if self.timer % (self.params.SIMULATION_TIME / 100) <= self.params.TIME_RESOLUTION:
@@ -170,17 +196,32 @@ class blockfaceNet:
     def park(self, block, carIndex):
         self.bface[block].total += 1
         available_spots = [ j for j, x in enumerate(self.all_spots[block]) if x <= self.bface[block].renege_rate + self.params.TIME_RESOLUTION ]
-        if len(available_spots) > 0:
+        garageProb = np.random.uniform(0,1)
+        if self.bface[block].garage == True and garageProb < self.params.GARAGE_PROB:
+            self.bface[block].parking_garage_rejects += 1
+        elif len(available_spots) > 0:
             car_park_time = np.random.exponential(self.bface[block].service_rate)
             self.all_spots[block][available_spots[0]] = car_park_time
             self.bface[block].served += 1
         else:
-            newDest = self.bface[block].neighbors[np.random.choice([ i for i in range(len(self.bface[block].neighbors)) ])]
-            newBface = newDest[1]
-            newBfaceIndex = newDest[0]
-            #fixed travel time is on
-            drive_time = self.bface[block].neighbor_streets[newBfaceIndex].get_travel_time(self.bface[newBface], fixed=self.params.DRIVE_TIME)
-            self.cars[carIndex].total_drive_time += drive_time
-            self.total_flow[block, newBface] += 1
-            self.streets[block][newBfaceIndex].append((carIndex, drive_time))
-            self.streets[block][newBfaceIndex].sort(key = lambda t: t[1])
+            try:
+                newDest = self.bface[block].neighbors[np.random.choice([ i for i in range(len(self.bface[block].neighbors)) ])]
+                newBface = newDest[1]
+                newBfaceIndex = newDest[0]
+                #fixed travel time is on
+                drive_time = self.bface[block].neighbor_streets[newBfaceIndex].get_travel_time(self.bface[newBface], fixed=self.params.DRIVE_TIME)
+                if self.params.BLOCKS_BEFORE_QUIT != 0.0:
+                    if self.cars[carIndex].total_drive_time > self.params.BLOCKS_BEFORE_QUIT * self.params.DRIVE_TIME:
+                        self.bface[block].parking_garage_rejects += 1
+                    else:
+                        self.cars[carIndex].total_drive_time += drive_time
+                        self.total_flow[block, newBface] += 1
+                        self.streets[block][newBfaceIndex].append((carIndex, drive_time))
+                        self.streets[block][newBfaceIndex].sort(key = lambda t: t[1])
+                else:
+                    self.cars[carIndex].total_drive_time += drive_time
+                    self.total_flow[block, newBface] += 1
+                    self.streets[block][newBfaceIndex].append((carIndex, drive_time))
+                    self.streets[block][newBfaceIndex].sort(key = lambda t: t[1])
+            except Exception as err:
+                self.bface[block].isolated_rejects += 1
