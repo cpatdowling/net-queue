7fa302f5c84f7266629bc4cecf9ad621ea104bac
cpatdowling <cpatdowling@gmail.com>
2017-04-13 15:28:57 -0700

    batch script version 2

--- src/python/qnet/nbus_batch.py
+++ src/python/qnet/nbus_batch.py
@@ -1,116 +1,20 @@
-import sys
-import os
-import numpy as np
 from qnet import *
-
-import pickle
 
 paramFilePath = sys.argv[1]
 saveDir = sys.argv[2]
+
 params = parameters()
 params.read(paramFilePath)
+params.VERBOSE = False
+params.STATS = ["occupancy", "traffic", "interrejection", "stationary"]
 
 #initialize network
-QNet = blockfaceNet(params, stats=["utilization", "traffic", "rejection", "stationary"])
+QNet = blockfaceNet(params)
 
-total_arrivals = 0
-  
-while QNet.timer < QNet.params.SIMULATION_TIME:
-    currently_arriving_blocks = [ j for j, x in enumerate(list(QNet.new_arrival_timer)) if x < QNet.params.TIME_RESOLUTION ]
-    if len(currently_arriving_blocks) > 0:
-        for i in currently_arriving_blocks:
-            total_arrivals += 1
-            blockindex = QNet.injection_map[i]
-            QNet.bface[blockindex].exogenous += 1
-            QNet.carIndex += 1
-            QNet.cars[QNet.carIndex] = car(QNet.carIndex)
-            QNet.park(blockindex, QNet.carIndex)
+#simulate
+QNet.simulate()
 
-            next_arrival_time = np.random.exponential(QNet.bface[blockindex].arrival_rate)
-            QNet.new_arrival_timer[i] = next_arrival_time        
-                   
-    for origin in QNet.bface.keys():
-        for dest in range(len(QNet.bface[origin].neighbors)):
-	        destblock = QNet.bface[origin].neighbors[dest][1]
-	        if len(QNet.streets[origin][dest]) > 0 and QNet.streets[origin][dest][0][1] < QNet.params.TIME_RESOLUTION:       
-		        while QNet.streets[origin][dest][0][1] < QNet.params.TIME_RESOLUTION:
-		            carIndex = QNet.streets[origin][dest][0][0]
-		            QNet.park(destblock, carIndex)
-		            QNet.streets[origin][dest].pop(0)
-		            if len(QNet.streets[origin][dest]) == 0:
-			            break
-    QNet.step_time(supress=True, debug=False)
-    
-#calculate network statistics
-utilizationStats = np.zeros((1,len(QNet.bface.keys())))
-avgInterRejection = np.zeros((1, len(QNet.bface.keys())))
-maxSpaces = int(np.max(QNet.params.NUM_SPOTS))
-stationaryDistributions = np.zeros((len(QNet.bface.keys()), maxSpaces+1))
-stationaryBins = range(maxSpaces+2) #+1 for when block is empty, +1 for open max interval in np.histogram, maxSpaces + 1 relevant values
-for i in QNet.bface.keys():
-    utilizationStats[0, i] = float(sum(QNet.bface[i].utilization))/float(len(QNet.bface[i].utilization))
-    statDist = list(np.histogram(QNet.bface[i].num_in_service, bins=stationaryBins, density=True)[0])
-    for k in range(len(statDist)):
-        stationaryDistributions[i, k] = statDist[k]
-    try:
-        avgInterRejection[0, i] = float(sum(QNet.bface[i].inter_reject_times))/float(len(QNet.bface[i].inter_reject_times))
-    except:
-        avgInterRejection[0, i] = 0.0
-    
-        
-#colate street traffic stats
-#this is super fuckin hacky, damn streets, need to make blocks and streets the same
-#queue class and then just instantiate them accordingly, access all information for
-#a block or a street from their class instance
-num_streets = 0
-for origin in range(len(QNet.streets_traffic.keys())):
-    for dest in range(len(QNet.streets_traffic[origin])):
-        num_streets += 1
-trafficStats = np.zeros((len(QNet.clock), 1+num_streets))
-for t in range(len(QNet.clock)):
-    trafficStats[t, 0] = QNet.clock[t]
-    for j in range(len(QNet.streets_traffic.keys())):
-        for k in range(len(QNet.streets_traffic[j])):
-            trafficStats[t, j+k+1] = QNet.streets_traffic[j][k][t]
-
-total = 0.0
-numCars = float(len(QNet.cars.keys()))
-for carInd in QNet.cars.keys():
-    total += QNet.cars[carInd].total_drive_time
-    #print(QNet.cars[carInd].bfaces_attempted) #potential error in wait times
-averageWait = np.array([[total/numCars]])
-
-
-
-
-
-
-#Write
-simulation_ID = str(np.random.randint(1,100000))
-
-np.savetxt(saveDir + "/traffic_total_flow_" + simulation_ID + ".txt", QNet.total_flow, delimiter=",")
-
-np.savetxt(saveDir + "/traffic_over_time_" + simulation_ID + ".txt", trafficStats, delimiter=",")
-
-np.savetxt(saveDir + "/utilization_" + simulation_ID + ".txt", utilizationStats, delimiter=",")
-
-np.savetxt(saveDir + "/avg_interrejection_" + simulation_ID + ".txt",
-avgInterRejection, delimiter = ",")
-
-np.savetxt(saveDir + "/avg_wait_" + simulation_ID + ".txt", averageWait, delimiter = ",")
-
-np.savetxt(saveDir + "/stationary_dist_" + simulation_ID + ".txt", stationaryDistributions, delimiter = ",")
-
-
-
-if False:        
-    #Print network statistics    
-    print("\nResults:\n\n")
-    print("System stats:\n")
-    print("\tTotal arrivals: " + str(total_arrivals) + "\n")
-    print("\tAverage wait time: " + str(averageWait) + "\n")
-    print("\tTotal traffic flow: \n" + str(QNet.total_flow) + "\n\n")
-    
-    print("Blockface stats:\n")
-    for i in range(len(QNet.bface.keys())):
-        print("\tBlockface " + str(i+1) + " utilization: " + str(utilizationStats[0, i]))
+#output
+rep = report(QNet, saveDir)
+rep.write_to_file()
+         
--- src/python/qnet/qnet.py
+++ src/python/qnet/qnet.py
@@ -3,7 +3,6 @@ import os
 import numpy as np
 
 #helper functions
-
 def tf(string): #for boolean parameter values
     if string == "False":
         return(False)
@@ -29,41 +28,45 @@ def sample_dist(distname, distparams):
         val = False
     return(val)
 
+
 class parameters:
     def __init__(self):
         #all parameter values
         self.ALL = ["SIMULATION_TIME", "TIME_RESOLUTION", "DRIVE_TIME_DIST", "DRIVE_TIME_DIST",
-                    "EXOGENOUS_INTERARRIVAL", "SERVICE_TIME_DIST", "RENEGE_TIME", "NUM_SPOTS", 
-                    "ROAD_NETWORK", "VERBOSE"]
+                    "EXOGENOUS_INTERARRIVAL", "SERVICE_TIME_DIST", "SERVICE_TIME", "RENEGE_TIME", "NUM_SPOTS", 
+                    "ROAD_NETWORK", "ARRIVAL_DIST", "VERBOSE"]
         #If param file passes single float, float is diagonalized across a network matrix
         self.diagonalize = ["EXOGENOUS_INTERARRIVAL", "SERVICE_TIME", "RENEGE_TIME", "NUM_SPOTS"]
 
         #Simulator parameters
         self.SIMULATION_TIME = 1000.0
-        self.TIME_RESOLUTION = 0.001
+        self.TIME_RESOLUTION = 0.01
         self.VERBOSE = True
         
         #Simulator statistics to collect, list of keywords
         self.STATS = [] 
+        self.STATS_OPTIONS = ["occupancy", "stationary", "interrejection", "traffic"]
             #"occupancy": measures proportion of servers in use at given time intervals
             #"stationary": collects data to estimate stationary distribution of queues
             #"interrejection": collects data to determine interreject time distribution for queue
             #"traffic": collects data on the number of cars on each street
-
+        self.STATS_OPTIONS_TIMER = ["occupancy", "stationary", "traffic"] #stats measured at intervals
+        
         #Network parameters
         self.EXOGENOUS_INTERARRIVAL = 1.5 
+        self.ARRIVAL_DIST = "exponential"
         self.SERVICE_TIME = 5.0
-        self.SERVICE_TIME_DIST = "fixed" #distribution for service times, eg exponential or fixed
+        self.SERVICE_TIME_DIST = "exponential" #distribution for service times, eg exponential or fixed
         self.RENEGE_TIME = 0.0           #amount of time driver will spend before quitting: not implemented
         self.NUM_SPOTS = 5
         self.DRIVE_TIME = 1.0
-        self.DRIVE_TIME_DIST = "exponential" #distribution for drive times, eg exponential or fixed
+        self.DRIVE_TIME_DIST = "fixed" #distribution for drive times, eg exponential or fixed
 
         #Directed network topologies
         #Default network is 2-cycle
         self.ROAD_NETWORK = np.array([[0,1],
                                       [1,0]])
-        self.networks = ["ROAD_NETWORK"] #list is hold over for multiple overlaid networks (parking garages)
+        self.networks = ["ROAD_NETWORK", "GARAGE_NETWORK"] #list is hold over for multiple overlaid networks (parking garages)
         
     def read(self, inFilePath):
         #read parameter file
@@ -119,12 +122,15 @@ class nqueue: #this queue class serves as the queue type for both streets and bl
         self.TIME_RESOLUTION = paramsInst.TIME_RESOLUTION
         self.ERR_LOG = {} #key is car index, val is error message
         if category == "blockface":
-            self.EXOGENOUS_INTERARRIVAL = paramsInst.EXOGENOUS_INTERARRIVAL
+            self.EXOGENOUS_INTERARRIVAL = paramsInst.EXOGENOUS_INTERARRIVAL[index,index]
+            self.NEW_ARRIVAL_TIMER = 0.0
+            self.NEW_ARRIVAL_DIST = paramsInst.ARRIVAL_DIST
             self.SERVICE_TIME = paramsInst.SERVICE_TIME[index, index]
             self.SERVICE_TIME_DIST = paramsInst.SERVICE_TIME_DIST
             self.NUM_SPOTS = paramsInst.NUM_SPOTS[index, index]
-            self.ACTIVE_SERVERS = np.zeros((int(self.NUM_SPOTS),))
+            self.ACTIVE_SERVERS = np.zeros((int(self.NUM_SPOTS),2))
             self.NEIGHBORING_BLOCKS = []
+            self.LOST = [] #list of car instances that were lost to the network
             
             #statistics
             self.TOTAL = 0
@@ -143,8 +149,8 @@ class nqueue: #this queue class serves as the queue type for both streets and bl
             self.SERVICE_TIME = paramsInst.DRIVE_TIME
             self.SERVICE_TIME_DIST = paramsInst.DRIVE_TIME_DIST
             self.NUM_SPOTS = np.inf #infinite server queue
-            self.ACTIVE_SERVERS = np.zeros((20,)) #assuming a likely maximum capacity, 
-                                     #will dynamically expand if needed
+            self.ACTIVE_SERVERS = np.array([[paramsInst.SIMULATION_TIME * 2, 0]]) #list will dynamically expand
+                                                                                  #"0th" car never finishes, holds list in place
             
             #statistics
             self.TOTAL = 0
@@ -161,15 +167,18 @@ class nqueue: #this queue class serves as the queue type for both streets and bl
         service_time = sample_dist(self.SERVICE_TIME_DIST, self.SERVICE_TIME)
         return(service_time)
     
+    def set_arrival_time(self):
+        self.NEW_ARRIVAL_TIMER = sample_dist(self.NEW_ARRIVAL_DIST, self.EXOGENOUS_INTERARRIVAL)
+    
     def get_num_in_service(self):
-        num = self.ACTIVE_SERVERS[self.ACTIVE_SERVERS > self.TIME_RESOLUTION].shape[0]
+        num = self.ACTIVE_SERVERS[self.ACTIVE_SERVERS[:,0] > self.TIME_RESOLUTION].shape[0]
         return(num)
     
     def get_available_servers(self):
         #for determining availability in finite capacity queues
-        available = [ j for j, x in enumerate(self.ACTIVE_SERVERS) if x <= self.TIME_RESOLUTION ]
+        available = [ j for j, x in enumerate(self.ACTIVE_SERVERS[:,0]) if x <= self.TIME_RESOLUTION ]
         if self.CATEGORY == "street" and len(available) == 0:
-            new_server_array = np.concatenate( (self.ACTIVE_SERVERS, np.zeros((1,))), axis=0)
+            new_server_array = np.concatenate( (self.ACTIVE_SERVERS, np.zeros((1,2))), axis=0)
             self.ACTIVE_SERVERS = new_server_array
             available = [self.ACTIVE_SERVERS.shape[0] - 1]
         return(available)
@@ -177,8 +186,8 @@ class nqueue: #this queue class serves as the queue type for both streets and bl
     def get_neighbor_block(self, carInst):
         #asks car instance for search strategy
         newDest = carInst.choose_next_block(self.NEIGHBORING_BLOCKS)
-        if newDest == False:
-            print("Isolated rejection occured at block-face: " + str(self.INDEX))
+        if newDest == None:
+            print("Isolated rejection occured at block-face, no adjacent blockfaces found by car strategy: " + str(self.INDEX))
         return(newDest)
     
     def new_car(self, carInst, current_time):
@@ -188,7 +197,8 @@ class nqueue: #this queue class serves as the queue type for both streets and bl
         #update servers
         stime = self.get_service_time()
         available_servers = self.get_available_servers()
-        self.ACTIVE_SERVERS[available_servers[0]] = stime
+        self.ACTIVE_SERVERS[available_servers[0], 0] = stime
+        self.ACTIVE_SERVERS[available_servers[0], 1] = carInst.INDEX
         
         if self.CATEGORY == "street":
             #update car stats
@@ -224,19 +234,19 @@ class car:
         self.SEARCH_STRATEGY = "uniform" #develop search strategy here, so next block can be chosen from queue class
                                          #according to this search strategy
             
-    def choose_next_block(neighbors):
+    def choose_next_block(self, neighbors):
         #choose from list of integers, neighbors, next block
         if len(neighbors) == 0:
-            print("Isolated rejection at network boundary (no out-degree neighbors)")
-            next_block = False
-        if self.SEARCH_STRATEGY == "uniform":
-            next_block = np.random.choice(neighbors)
+            print("Isolated rejection at network boundary (no out-degree neighbors found at blockface)")
+            next_block = None
+        else:
+            if self.SEARCH_STRATEGY == "uniform":
+                next_block = np.random.choice(neighbors)
         return(next_block)
 
 class blockfaceNet:
-    def __init__(self, paramInstance, stats=[]):
+    def __init__(self, paramInstance):
         self.PARAMS = paramInstance
-        self.PARAMS.STATS = stats
         self.TIMER = 0.0
         self.STOPWATCH = []
         self.NUM_MEASUREMENTS = 1000.0 #number of measurements for in situ sensors
@@ -251,11 +261,10 @@ class blockfaceNet:
         #new arrival timer        
         arrival_times = np.diag(self.PARAMS.EXOGENOUS_INTERARRIVAL)
         self.INJECTION_BLOCKS = [ i for i, x in enumerate(arrival_times) if float(x) != 0.0 ]
-        self.NEW_ARRIVAL_TIMER = np.zeros((len(self.INJECTION_BLOCKS),))
         for bi in range(len(self.INJECTION_BLOCKS)):
             curr = self.INJECTION_BLOCKS[bi]
             inittime = self.BLOCKFACES[curr].get_service_time()
-            self.NEW_ARRIVAL_TIMER[bi] = inittime   
+            self.BLOCKFACES[curr].NEW_ARRIVAL_TIMER = inittime   
         
         self.STREETS = {} #key is origin blockface, value is dict of streets with 
                           #destinations by key
@@ -263,16 +272,25 @@ class blockfaceNet:
             self.STREETS[origin] = {}
             for dest_n in self.BLOCKFACES[origin].NEIGHBORING_BLOCKS:
                 self.STREETS[origin][dest_n] = nqueue("street", (origin,dest_n), self.PARAMS)
-                
+        
+        self.CAR_COUNT = 1
         self.CARS = {} #index keys to car object values, keeps track of all arrivals to system
-    
+        self.BLOCKFACE_LIST = sorted(self.BLOCKFACES.values(), key=lambda x: x.INDEX)
+        self.STREET_LIST = []
+        for origin in range(num_blocks):
+            for dest in self.STREETS[origin].keys():
+                self.STREET_LIST.append(self.STREETS[origin][dest])
+        self.STREET_LIST.sort(key=lambda x: (x.ORIGIN, x.DESTINATION))
+        
     def step_time(self):
         self.TIMER += self.PARAMS.TIME_RESOLUTION
         #countdowns
         for block in self.BLOCKFACES.keys():
-            self.BLOCKFACES[block].ACTIVE_SERVERS -= self.PARAMS.TIME_RESOLUTION
+            self.BLOCKFACES[block].ACTIVE_SERVERS[:,0] -= self.PARAMS.TIME_RESOLUTION
+            if block in self.INJECTION_BLOCKS:
+                self.BLOCKFACES[block].NEW_ARRIVAL_TIMER -= self.PARAMS.TIME_RESOLUTION
             for st in self.STREETS[block]: #streets originating from the current block
-                self.STREETS[block][st].ACTIVE_SERVERS -= self.PARAMS.TIME_RESOLUTION
+                self.STREETS[block][st].ACTIVE_SERVERS[:,0] -= self.PARAMS.TIME_RESOLUTION
                 
         if self.TIMER % (self.PARAMS.SIMULATION_TIME / 10) <= self.PARAMS.TIME_RESOLUTION:
             if self.PARAMS.VERBOSE == True:
@@ -282,18 +300,66 @@ class blockfaceNet:
         if self.TIMER % (self.PARAMS.SIMULATION_TIME / self.NUM_MEASUREMENTS) <= self.PARAMS.TIME_RESOLUTION:
             self.STOPWATCH.append(self.TIMER)
             self.collect_timer_stats()
-            
-            
+        
+    def get_car_arrivals(self, nqueue_inst):
+        if nqueue_inst.CATEGORY == "street":
+            curr_arr = [ j for j, x in enumerate(list(nqueue_inst.ACTIVE_SERVERS[:,0])) if x < self.PARAMS.TIME_RESOLUTION ]
+            ret_arr = list(nqueue_inst.ACTIVE_SERVERS[curr_arr,1])
+            nqueue_inst.ACTIVE_SERVERS = np.delete(nqueue_inst.ACTIVE_SERVERS, curr_arr, axis=0)
+        elif nqueue_inst.CATEGORY == "blockface": #check arrival timer, assign new car index and add it to car dict
+            ret_arr = []
+            if nqueue_inst.INDEX in self.INJECTION_BLOCKS:
+                if nqueue_inst.NEW_ARRIVAL_TIMER < self.PARAMS.TIME_RESOLUTION:
+                    self.CARS[self.CAR_COUNT] = car(self.CAR_COUNT)
+                    ret_arr.append(self.CAR_COUNT)
+                    self.CARS[self.CAR_COUNT].ARRIVAL_TIME = self.TIMER
+                    self.CAR_COUNT += 1
+                    nqueue_inst.set_arrival_time()
+            else:
+                pass
+        else:
+            pass
+        return(ret_arr)
+        
+    def check_arrivals(self, category):
+        #returns a list of car instance indices
+        if category=="blockface":
+            all_arr = [ self.get_car_arrivals(block_inst) for block_inst in self.BLOCKFACE_LIST ]
+        elif category=="street":
+            all_arr = [ self.get_car_arrivals(street_inst) for street_inst in self.STREET_LIST ]
+        elif category=="garage":
+            all_arr = []
+        arrival = any([ any(i) for i in all_arr ])
+        if not arrival:
+            all_arr = False
+        return(all_arr) 
+    
+    def get_all_arrivals(self, category):
+        #retrieve arrival blockface belonging to car, list of tuples
+        #(arriving blockface, carInst)
+        arr = self.check_arrivals(category)
+        parking_cars = []
+        if arr == False:
+            pass
+        elif category=="blockface":
+            for i in range(len(arr)):
+                blocki = self.BLOCKFACE_LIST[i].INDEX
+                for cari in arr[i]:
+                    parking_cars.append( (blocki, int(cari)) )
+        elif category=="street":
+            for i in range(len(arr)):
+                dest = self.STREET_LIST[i].DESTINATION
+                for cari in arr[i]:
+                    parking_cars.append( (dest, int(cari)) )
+        elif category=="garage":
+            print("no garage arrivals")
+            pass
+        return(parking_cars)
+    
     def collect_timer_stats(self):
-        #stats collected at times recorded by self.STOPWATCH, to few to bother with options function
-        if "occupancy" in self.PARAMS.STATS:
-            self.update_occupancy()
-                
-        if "stationary" in self.PARAMS.STATS:
-            self.update_stationary()
-            
-        if "traffic" in self.PARAMS.STATS:
-            self.update_traffic()
+        for stat in self.PARAMS.STATS_OPTIONS_TIMER:
+            if stat in self.PARAMS.STATS and stat != "interrejection":
+                getattr(self, "update_" + stat)()
             
     #simulation statistics for blockfaces
     #STOPWATCH determined statistics
@@ -317,10 +383,10 @@ class blockfaceNet:
     #event determined statistics (measured when it happens)
     def update_interrejection(self, block):
         #updating rejection time sensor and appending rejection times
-        last_reject = self.BLOCKFACE[block].LAST_REJECT_TIME
+        last_reject = self.BLOCKFACES[block].LAST_REJECT_TIME
         inter_reject = self.TIMER - last_reject
-        self.BLOCKFACE[block].INTERREJECTION_TIMES.append(inter_reject)
-        self.BLOCKFACE[block].LAST_REJECT_TIME = self.TIMER
+        self.BLOCKFACES[block].INTERREJECTION_TIMES.append(inter_reject)
+        self.BLOCKFACES[block].LAST_REJECT_TIME = self.TIMER
     
     def park(self, block, carIndex):
         available_spots = self.BLOCKFACES[block].get_available_servers()        
@@ -331,9 +397,86 @@ class blockfaceNet:
                 self.update_interrejection(block)
             try:
                 next_blockface = self.BLOCKFACES[block].get_neighbor_block(self.CARS[carIndex]) #takes car as argument for search strategy
-                self.STREETS[block][next_blockface].new_car(self.CARS[carIndex], self.TIMER) #update queue with new car
+                if next_blockface == None:
+                    self.BLOCKFACES[block].LOST.append(self.CARS[carIndex])
+                else:
+                    self.STREETS[block][next_blockface].new_car(self.CARS[carIndex], self.TIMER) #update queue with new car
             except Exception as err:
                 print("Isolated reject error at blockface " + str(block))
                 print(err)
                 self.BLOCKFACES[block].ERR_LOG[carIndex] = err
+                
+    def simulate(self):
+        while self.TIMER < self.PARAMS.SIMULATION_TIME:
+            b_arrivals = self.get_all_arrivals("blockface")
+            if b_arrivals:
+                for tup in b_arrivals:
+                    self.park(tup[0], tup[1])
+            s_arrivals = self.get_all_arrivals("street")
+            if s_arrivals:
+                for tup in s_arrivals:
+                    self.park(tup[0], tup[1])
+            self.step_time()
 
+class report:
+    def __init__(self, queuenet_inst, outputdir=os.getcwd()):
+        self.SIM_ID = str(np.random.randint(1,100000))
+        self.QNET = queuenet_inst
+        if outputdir[-1] != "/":
+            outputdir += "/"
+        self.OUTPUT_DIRECTORY = outputdir
+        self.BLOCKS = sorted(queuenet_inst.BLOCKFACES.keys())
+        pass
+    
+    def file_name(self, label):
+        out = self.OUTPUT_DIRECTORY + "/" + label + "_" + self.SIM_ID + ".txt"
+        return(out)
+    
+    def traffic_array(self):
+        traffarray = np.zeros(self.QNET.PARAMS.ROAD_NETWORK.shape)
+        for origin in sorted(self.QNET.STREETS.keys()):
+            for dest in sorted(self.QNET.STREETS[origin].keys()):
+                traff = np.mean(np.asarray(self.QNET.STREETS[origin][dest].TRAFFIC)-1)
+                traffarray[origin,dest] = traff
+        return(traffarray)
+        
+    def occupancy_array(self):
+        occup = []
+        for block in self.BLOCKS:
+            occup.append(np.mean(np.asarray(self.QNET.BLOCKFACES[block].OCCUPANCY)))
+        return(np.asarray(occup))
+        
+    def stationary_array(self):
+        maxSpaces = int(np.max(self.QNET.PARAMS.NUM_SPOTS))
+        statDists = np.zeros((len(self.QNET.BLOCKFACES.keys()), maxSpaces + 1))
+        statBins = range(maxSpaces+2)
+        for block in self.BLOCKS:
+            statDist = np.histogram(self.QNET.BLOCKFACES[block].STATIONARY, 
+                                    bins=statBins, density=True)[0]
+            statDists[block] = statDist
+        return(statDist)
+    
+    def interrejection_array(self):
+        inter = []
+        for block in self.BLOCKS:
+            inter.append(np.mean(np.asarray(self.QNET.BLOCKFACES[block].INTERREJECTION_TIMES)))
+        return(np.asarray(inter))
+    
+    def write_to_file(self):
+        #write data to files in specifed output directory based on stats collected
+        stats = self.QNET.PARAMS.STATS_OPTIONS
+        for stat in stats:
+            if stat in self.QNET.PARAMS.STATS:
+                data = getattr(self, stat + "_array")()
+                np.savetxt(self.file_name(stat), data, delimiter=",")
+        
+    def screen_print(self):
+        import pprint
+        stats = self.QNET.PARAMS.STATS_OPTIONS
+        for stat in stats:
+            if stat in self.QNET.PARAMS.STATS:
+                data = getattr(self, stat + "_array")()
+            print("======" + stat.upper() + "======")
+            pprint.pprint(data)
+            print("\n\n")
+        pass
